52df39bc79cb5a1060d65927b637c1ce344cd9ff
Markus Kapp <mkapp1999@gmail.com>
2025-08-26 08:58:13 +0200

    WIP

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,11 @@
 import Calculator from "./components/Calculator.vue";
 import Explanation from "@/components/Explanation.vue";
 import Store from "@/components/Store.vue";
+import ProzentRechnen from "@/components/ProzentRechnen.vue";
 </script>
 
 <template>
-  <Store />
+  <ProzentRechnen />
 </template>
 
 <style scoped>
---
+++ src/components/ProzentRechnen.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+const number1 = ref(0)
+const number2 = ref(0)
+const operator = ref('+')
+
+if (operator.value === '+') {
+  add(number1.value, number2.value)
+ }
+
+</script>
+
+<template>
+  <q-layout>
+    <q-page-container>
+      <q-page class="flex flex-center">
+        <div class="column items-center">
+          <div class="row q-mb-lg">
+            <h3>Dein Ergebnis ist: {{ result }}</h3>
+          </div>
+
+          <div class="row q-mb-lg">
+            <q-input
+                class="q-mr-md"
+                outlined
+                type="number"
+                v-model.number="number1"
+                label="Zahl "
+            />
+            <q-input
+                class="q-mr-md"
+                outlined
+                type="number"
+                v-model.number="number2"
+                label="prozent mehr"
+            />
+            <q-input
+                outlined
+                type="number"
+                v-model.number="zwischenErgebnis"
+                label="zwischen Ergebnis"
+            />
+          </div>
+
+
+
+          <q-btn color="primary" @click="calculate">Berechnen</q-btn>
+        </div>
+      </q-page>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<style scoped>
+
+</style>
